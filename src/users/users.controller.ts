bfda565b1fe3bd63c0ed4d9cba2c908dbe29f5de
accm <com.michaell@gmail.com>
2020-09-21 17:57:51 +0300

    add Joi validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JoiCreateUserPipe } from '../helpers/pipes/create-user.pipe';
+import { createUserSchema } from './dto/create-user.schema';
+import { CreateUserProfilesDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  async create(@Body() props) {
+  @UsePipes(new JoiCreateUserPipe(createUserSchema))
+  async create(@Body() props: CreateUserProfilesDto) {
     const propsUser = props.user
     const propsProfile = props.profile
     return this.usersService.create(propsUser, propsProfile);
